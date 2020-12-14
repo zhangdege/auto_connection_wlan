dd0434f42ce8916f09a07781c3282ddc00900990
zhangdege <908481391@qq.com>
2020-12-14 21:19:22 +0800

    update
    update

--- 校园网脚本/WLANAUTO/connect.js
+++ 校园网脚本/WLANAUTO/connect.js
@@ -2,13 +2,14 @@ const os = require('os');
 const fetch = require('node-fetch');
 const process = require('process');
 
-//确保下面body里的userid="学号"和passwd="你的密码"的正确
-//我的网址参数：http://111.6.96.83:9999/portalReceiveAction.do?wlanuserip=10.200.151.164&wlanacname=XXU-A07-4F-ME60
-//  wlanacname=XXU-A07-4F-ME60这个也是要修改的
-//userid  with password也要修改
+const userid = "20174103131";   //记得改学号
+const useridtemp = "20174103131";  //这里也填着学号
+const wlanacname = "XXU-A07-4F-ME60";   // wlanacname=XXU-A07-4F-ME60这个也是要修改的 参考这里  http://111.6.96.83:9999/portalReceiveAction.do?wlanuserip=10.200.151.164&wlanacname=XXU-A07-4F-ME60
+const passwd = "你的密码";   //填写密码
+const operator="%40xxxylt";  // operator: %40xxxyyd移动    %40xxxylt联通   %40xxxydx电信
 
 function getIPAdresss() {
-  var interfaces = os.networkInterfaces();
+  const interfaces = os.networkInterfaces();
   let WLAN_interface = interfaces.WLAN;
   for (var devName in WLAN_interface) {
     var iface = WLAN_interface[devName];
@@ -18,7 +19,6 @@ function getIPAdresss() {
   }
 }
 
-//不知道没什么没有获取到数据
 function getConnect(){
 	const myotherip = getIPAdresss();
 	fetch("http://111.6.96.83:9999/portalAuthAction.do", {
@@ -30,18 +30,20 @@ function getConnect(){
 			"upgrade-insecure-requests": "1",
 			"cookie": "JSESSIONID=D3812F31C353C7379689CE5DE8BBEB21.worker4"
 		  },
-		  "referrer": "http://111.6.96.83:9999/portalReceiveAction.do?wlanuserip="+myotherip+"&wlanacname=XXU-A07-4F-ME60",
+		  "referrer": "http://111.6.96.83:9999/portalReceiveAction.do?wlanuserip="+myotherip+"&wlanacname="+wlanacname+"",
 		  "referrerPolicy": "strict-origin-when-cross-origin",
-		  "body": "wlanuserip="+myotherip+"&wlanacname=XXU-A07-4F-ME60&chal_id=&chal_vector=&auth_type=PAP&seq_id=&req_id=&wlanacIp=117.159.38.130&ssid=&vlan=&mac=&message=&bank_acct=&isCookies=&version=0&authkey=88----89&url=&usertime=0&listpasscode=0&listgetpass=0&getpasstype=0&randstr=5997&domain=&isRadiusProxy=true&usertype=0&isHaveNotice=0&times=12&weizhi=0&smsid=&freeuser=&freepasswd=&listwxauth=0&templatetype=1&tname=xxxy_pc_portal_V2.0&logintype=0&act=&is189=false&terminalType=&checkterminal=true&portalpageid=124&listfreeauth=0&viewlogin=1&userid=你的学号%40xxxylt&authGroupId=&useridtemp=你的学号%40xxxylt&passwd=你的密码&operator=%40xxxylt",
+		  "body": "wlanuserip="+myotherip+"&wlanacname="+wlanacname+"&chal_id=&chal_vector=&auth_type=PAP&seq_id=&req_id=&wlanacIp=117.159.38.130&ssid=&vlan=&mac=&message=&bank_acct=&isCookies=&version=0&authkey=88----89&url=&usertime=0&listpasscode=0&listgetpass=0&getpasstype=0&randstr=5997&domain=&isRadiusProxy=true&usertype=0&isHaveNotice=0&times=12&weizhi=0&smsid=&freeuser=&freepasswd=&listwxauth=0&templatetype=1&tname=xxxy_pc_portal_V2.0&logintype=0&act=&is189=false&terminalType=&checkterminal=true&portalpageid=124&listfreeauth=0&viewlogin=1&userid="+userid+operator+"&authGroupId=&useridtemp="+useridtemp+operator+"&passwd="+passwd+"&operator="+operator+"",
 		  "method": "POST",
 		  "mode": "cors"
-	}).then(res=>{
-		console.log(res)
 	});
 }
 getConnect();
 
 //2秒后自动退出
+
 setTimeout(()=>{
+	console.log("自动认证联网成功！如果此时已经自动打开浏览器请关闭浏览器！")  //做个样子，都会输出
 	process.exit(0);
 },2000)
+
+
